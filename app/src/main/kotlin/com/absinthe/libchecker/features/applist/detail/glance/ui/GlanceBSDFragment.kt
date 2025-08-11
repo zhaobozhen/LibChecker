e2081b599ca310d7b748767f11df2b6201ccf488
Absinthe <zhaobozhen2025@gmail.com>
2025-08-11 23:03:03 +0800

    Draw capsules.

--- app/src/main/kotlin/com/absinthe/libchecker/features/applist/detail/glance/ui/GlanceBSDFragment.kt
+++ app/src/main/kotlin/com/absinthe/libchecker/features/applist/detail/glance/ui/GlanceBSDFragment.kt
@@ -5,11 +5,13 @@ import androidx.core.graphics.drawable.toBitmap
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.lifecycleScope
 import androidx.palette.graphics.Palette
+import com.absinthe.libchecker.R
 import com.absinthe.libchecker.app.SystemServices
 import com.absinthe.libchecker.features.applist.detail.glance.ui.view.GlanceBSDView
 import com.absinthe.libchecker.features.applist.detail.ui.EXTRA_PACKAGE_NAME
 import com.absinthe.libchecker.utils.PackageUtils
 import com.absinthe.libchecker.utils.UiUtils
+import com.absinthe.libchecker.utils.extensions.copyToClipboard
 import com.absinthe.libchecker.utils.extensions.getAppName
 import com.absinthe.libchecker.utils.extensions.getVersionCode
 import com.absinthe.libchecker.utils.extensions.putArguments
@@ -34,12 +36,39 @@ class GlanceBSDFragment : BaseBottomSheetViewDialogFragment<GlanceBSDView>() {
       val icon = pi.applicationInfo!!.loadIcon(SystemServices.packageManager).toBitmap()
       val palette = Palette.from(icon).generate()
       val color = UiUtils.desaturateColor(palette.dominantSwatch!!.rgb, 0.618f)
+      root.image.setOnLongClickListener { view ->
+        root.image.copyToClipboard()
+        true
+      }
+
       withContext(Dispatchers.Main) {
         val info = GlanceBSDView.Info(
           appName = pi.getAppName() ?: "null",
           packageName = packageName,
           versionName = pi.versionName ?: "null",
-          versionCode = pi.getVersionCode()
+          versionCode = pi.getVersionCode(),
+          capsules = listOf(
+            GlanceBSDView.CapsuleInfo(
+              R.drawable.ic_gradle,
+              "Gradle",
+              "8.8"
+            ),
+            GlanceBSDView.CapsuleInfo(
+              com.absinthe.lc.rulesbundle.R.drawable.ic_lib_android,
+              "Android",
+              "15"
+            ),
+            GlanceBSDView.CapsuleInfo(
+              com.absinthe.lc.rulesbundle.R.drawable.ic_lib_kotlin,
+              "Kotlin",
+              "1.9.20"
+            ),
+            GlanceBSDView.CapsuleInfo(
+              com.absinthe.lc.rulesbundle.R.drawable.ic_lib_kotlin,
+              "Kotlin",
+              "1.9.20"
+            )
+          )
         )
         root.drawImage(info, color)
       }
